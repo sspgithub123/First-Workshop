0ddd3fd813d7b292ed0230af1891e947618530c7
Shubham <ssp671995@gmail.com>
2022-03-09 23:54:31 +0530

    final use case1

---
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gambler;
+
+public class Gambler {
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("******* Welcome to Gambler Game *******");
+    }
+}
